f265700c395360c8eb7fd06edce79f7813580efe
Igor Tsarev <iam@tio.so>
2016-10-21 17:20:57 +0300

    Added 'delete duplicate files' action

--- find_duplicates.py
+++ find_duplicates.py
@@ -24,9 +24,10 @@ def _collapse_lonely(tree):
     return {k: v for (k, v) in tree.items() if len(v) > 1}
 
 
-class Comparator():
-    def __init__(self, dirs, verbose):
+class Comparator(object):
+    def __init__(self, dirs, verbose, delete):
         self.dirs = dirs
+        self.delete = delete
         debug_level = logging.ERROR
         if verbose:
             debug_level = logging.INFO
@@ -38,24 +39,22 @@ class Comparator():
         self.logger = logging.getLogger()
 
     def compare(self):
-        files = self.get_files()
-        count = self.print_dupes(
-            # compare files by hash. Very slow
-            self.compare_by_hash(
-                # compare files by slice of bytes from file. Fast
+        """ Run comparing files
+        """
+        dupes = self.print_dupes(
+            self.by_hash(
                 self.by_same_chunk(
-                    # compare files by their sizes. Very fast
-                    self.by_same_size(files)
+                    self.by_same_size(
+                        self.get_files()
+                    )
                 )
             )
         )
-        self.logger.info(
-            "Total duplicates: {} (without original file)".format(count)
-        )
+        if self.delete:
+            self.delete_dupes(dupes)
 
     def get_files(self):
         """ Get all files recursively from dirs
-        :param dirs: list of directories
         """
         file_tree = defaultdict(list)
         count = 0
@@ -68,6 +67,8 @@ class Comparator():
         return file_tree
 
     def by_same_size(self, file_tree):
+        """ Compare files by their sizes. Very fast
+        """
         size_tree = defaultdict(list)
         for file_path in _flat_tree(file_tree):
             size_tree[os.path.getsize(file_path)].append(file_path)
@@ -78,9 +79,7 @@ class Comparator():
         return filtered
 
     def by_same_chunk(self, file_tree, chunk_size=8):
-        """
-        :param files:
-        :return:
+        """ Compare files by slice of bytes from file. Fast
         """
         chunk_tree = defaultdict(list)
 
@@ -88,7 +87,7 @@ class Comparator():
             prev_chunk = None
             for file_name in files:
                 with open(file_name, 'rb') as f:
-                    f.seek(int(size/2))  # Center of file
+                    f.seek(int(size / 2))  # Center of file
                     chunk = f.read(chunk_size)
                     if prev_chunk is None or prev_chunk == chunk:
                         chunk_tree[size].append(file_name)
@@ -99,20 +98,38 @@ class Comparator():
         ))
         return filtered
 
-    def compare_by_hash(self, file_tree):
+    def by_hash(self, file_tree):
+        """ Compare files by hash. Very slow
+        """
         hash_tree = defaultdict(list)
         for file_path in _flat_tree(file_tree):
             with open(file_path, 'rb') as f:
                 hash_tree[md5(f.read()).hexdigest()].append(file_path)
         return _collapse_lonely(hash_tree)
 
-    def print_dupes(self, dupes):
+    def print_dupes(self, file_tree):
+        """ Print duplicates
+        """
         dupes_count = 0
-        for file_hash, files in dupes.items():
+        for file_hash, files in file_tree.items():
             for file_name in files:
                 print("{} duplicate {}".format(file_hash, file_name))
-            dupes_count += len(files)-1
-        return dupes_count
+            dupes_count += len(files) - 1
+        self.logger.info(
+            "Excess files: {}".format(dupes_count)
+        )
+        return file_tree
+
+    def delete_dupes(self, file_tree):
+        """ Delete duplicate files without original (first file in list)
+        """
+        for file_hash, files in file_tree.items():
+            self.logger.info("Deleting dupes of [{}]: {}".format(
+                files[0], ", ".join(files[1:])
+            ))
+            for file_name in files[1:]:
+                os.unlink(file_name)
+        return file_tree
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -120,9 +137,9 @@ if __name__ == '__main__':
     )
     parser.add_argument('dirs', metavar='D', type=str, nargs='+',
                         help='directories to search')
-    parser.add_argument('-v', '--verbose', type=bool, help="Be verbose",
-                        default=False)
+    parser.add_argument('-v', '--verbose', help="Be verbose",
+                        action='store_true')
+    parser.add_argument('-d', '--delete', help="Delete found duplicates",
+                        action='store_true')
     comparator = Comparator(**vars(parser.parse_args()))
     comparator.compare()
-
-
